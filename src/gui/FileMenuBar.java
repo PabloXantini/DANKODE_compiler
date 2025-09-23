6f6ff0726c0e1fbd1ab78e3183b64a3219a59810
PabloXantini <santinypabloe130@gmail.com>
2025-09-22 20:43:04 -0600

    style(gui): added button-compile-icon for UI

--- src/gui/FileMenuBar.java
+++ src/gui/FileMenuBar.java
@@ -4,28 +4,37 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-import ide.actions_tools.analysis.CompileAction;
+import gui.components.IDEButtonUI;
 
 public class FileMenuBar extends JPanel{
     //Colors
     private Color background;
     private Color colbtn1;
+    private Color colicon;
+    //Icons
+    private ImageIcon icon1;
     //Components
-    private JButton btnCompile;
+    private IDEButtonUI btnCompile;
     //Method stuff
-    private void styleMenuButton(JButton btn){
+    private void styleMenuButton(IDEButtonUI btn){
         btn.setBackground(colbtn1);
-        btn.setPreferredSize(new Dimension(40,40));
+        btn.setPreferredSize(new Dimension(30,30));
+        btn.setBorder(null);
+        btn.setIconColor(colicon);
     }
     public FileMenuBar(){
         background = new Color(255, 255, 255);
         colbtn1 = new Color(255, 255, 255);
-        btnCompile = new JButton();
+        colicon = new Color(102, 178, 255);
+        icon1 = new ImageIcon("res/icons/compile.png");
+        btnCompile = new IDEButtonUI(icon1);
         //Structuring
-        setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 8, 8));
         add(btnCompile);
         //Styles
         setBackground(background);
--- src/gui/components/IDEButtonUI.java
+++ src/gui/components/IDEButtonUI.java
@@ -1,7 +1,11 @@
 package gui.components;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
 import javax.swing.ImageIcon;
@@ -14,6 +18,18 @@ public class IDEButtonUI extends JButton{
         super();
         image = icon.getImage();
         img_observer = icon.getImageObserver();
+    }
+    public void setIconColor(Color color){
+        //Post-process the image
+        BufferedImage buffered_img = new BufferedImage(image.getWidth(img_observer), image.getHeight(img_observer), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = buffered_img.createGraphics();
+        g2d.drawImage(image, 0, 0, img_observer);
+        g2d.setComposite(AlphaComposite.SrcAtop);
+        g2d.setColor(color);
+        g2d.fillRect(0, 0, buffered_img.getWidth(), buffered_img.getHeight());
+        g2d.dispose();
+        image = buffered_img;
+        repaint();
     }
     @Override
     public void paint(Graphics g) {
