9acff4391259e893a8ecfcf8f772d2b5b6184b5d
PabloXantini <santinypabloe130@gmail.com>
2025-10-05 13:25:21 -0600

    fix(errors): Change type for code, for avoid negatives

--- src/dankcompiler/errors/TokenError.java
+++ src/dankcompiler/errors/TokenError.java
@@ -1,13 +1,13 @@
 package dankcompiler.errors;
 
 public class TokenError {
-    public final int code;
+    public final long code;
     public final TokenErrorType type;
     public final String lexem;
     public final int line;
     public final int column;
     public final TokenErrorCode errorcode;
-    public TokenError(int code, TokenErrorType type, String lexem, int line, int column, TokenErrorCode errorcode){
+    public TokenError(long code, TokenErrorType type, String lexem, int line, int column, TokenErrorCode errorcode){
         this.code = code;
         this.type = type;
         this.lexem = lexem;
--- src/dankcompiler/errors/TokenErrorHandler.java
+++ src/dankcompiler/errors/TokenErrorHandler.java
@@ -11,13 +11,14 @@ public class TokenErrorHandler {
     //Error Sequence
     private static int errornum = 0;
     //Method stuff
-    private static int generateCode(TokenErrorType type, TokenErrorCode errorcode){
-        int new_code = Objects.hash(type, errorcode, errornum);
+    private static long generateCode(TokenErrorType type, TokenErrorCode errorcode){
+        int hashed = Objects.hash(type, errorcode, errornum);
+        long new_code = Integer.toUnsignedLong(hashed);
         errornum++;
         return new_code;
     }
     public static TokenError generateError(String lexem, TokenErrorType type, int line, int column, TokenErrorCode errorcode){
-        int codetoken = generateCode(type, errorcode);
+        long codetoken = generateCode(type, errorcode);
         return new TokenError(codetoken, type, lexem, line, column, errorcode);
     }
     //THIS IS WHEN YOU CALL IT AS AN OBJECT
