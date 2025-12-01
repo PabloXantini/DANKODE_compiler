43ba341f9b9ece04cfb5152c3f9049d6c81cfb72
PabloXantini <santinypabloe130@gmail.com>
2025-11-30 22:39:08 -0600

    refactor(ast): changed reinstancing for ASTGeneralVisitor to iterator

--- src/dankcompiler/parsing/ast/ASTGeneralVisitor.java
+++ src/dankcompiler/parsing/ast/ASTGeneralVisitor.java
@@ -1,10 +1,17 @@
 package dankcompiler.parsing.ast;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class ASTGeneralVisitor implements ASTVisitor {
 	@Override
 	public Node visit(GroupNode groupnode) {
+		Iterator<Node> it = groupnode.getChildren().iterator();
+		while(it.hasNext()) {
+			Node node = it.next().accept(this);
+			if(node == null) it.remove();
+		}
+		/*
 		ArrayList<Node> children = new ArrayList<Node>();
 		for(Node child : groupnode.getChildren()) {
 			Node node = child.accept(this);
@@ -12,6 +19,7 @@ public abstract class ASTGeneralVisitor implements ASTVisitor {
 		}
 		if(children.isEmpty()) return null;
 		groupnode.setChildren(children);
+		*/
 		return groupnode;
 	}
 }
